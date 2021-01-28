e876baaf60b842490dbdc13e6303d5e5c944077c
Anthony Guzman <agl0210@gmail.com>
2021-01-28 07:10:40 -0500

    archivo de configuracion y layout html para estructura

--- manage.py
+++ manage.py
@@ -3,7 +3,12 @@ from myapp import create_app
 #permite levantar el servidor apartir de una instancia
 from flask_script import Manager
 
-myapp = create_app()
+from config import config
+
+
+config_class = config['development']
+
+myapp = create_app(config_class)
 
 if __name__ == '__main__':
     manager = Manager(myapp)
--- myapp/views.py
+++ myapp/views.py
@@ -5,7 +5,11 @@ from flask import render_template
 
 page = Blueprint('page', __name__,)
 
+#decorador para ejecutar la funcion, con el codigo 404
+@page.app_errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
 
 @page.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='bogota')
